e50795e367c2435187e02c602d3c231d40bc9639
Daiki Onodera <daikionodera0101@gmail.com>
2021-01-15 10:57:01 +0900

    windows package

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,22 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-bluetoothle.BluetoothLe",
+      "file": "plugins/cordova-plugin-bluetoothle/www/bluetoothle.js",
+      "pluginId": "cordova-plugin-bluetoothle",
+      "clobbers": [
+        "window.bluetoothle"
+      ]
+    },
+    {
+      "id": "cordova-plugin-bluetoothle.BluetoothLEPlugin",
+      "file": "plugins/cordova-plugin-bluetoothle/src/windows/BluetoothLEPlugin.js",
+      "pluginId": "cordova-plugin-bluetoothle",
+      "runs": true
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-bluetoothle": "6.2.0",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
+});
